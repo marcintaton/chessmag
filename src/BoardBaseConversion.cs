d356ed7a0ebd630a1fdbbb2a7052283b1e98e8a6
MArcin Tatoń <tatonmarcinpr@gmail.com>
2023-04-15 17:41:50 +0200

    conversion between 120 based and 64 based board indexing

---
+++ src/BoardBaseConversion.cs
@@ -0,0 +1,36 @@
+using chessmag.src.defs;
+using File = chessmag.src.defs.File;
+
+namespace chessmag.src
+{
+    public static class BoardBaseConversion
+    {
+        public static int[] Board64to120 { get; set; } = Enumerable.Repeat(120, 64).ToArray();
+
+        public static int[] Board120to64 { get; set; } = Enumerable.Repeat(65, Constants.BoardSize).ToArray();
+
+        public static int FrTo64(int file, int rank)
+        {
+            return file + (rank * 8);
+        }
+
+        public static int FrTo120(int file, int rank)
+        {
+            return 21 + file + (rank * 10);
+        }
+
+        static BoardBaseConversion()
+        {
+            for (int rank = (int)Rank._1; rank <= (int)Rank._8; ++rank)
+            {
+                for (int file = (int)File.a; file <= (int)File.h; ++file)
+                {
+                    int sq120 = FrTo120(file, rank);
+                    int sq64 = FrTo64(file, rank);
+                    Board64to120[sq64] = sq120;
+                    Board120to64[sq120] = sq64;
+                }
+            }
+        }
+    }
+}
--- src/Main.cs
+++ src/Main.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("This is where the fun begins!");
+﻿using chessmag.src;
+
+namespace chessmag
+{
+    public static class ChessMag
+    {
+        private static void Main()
+        {
+            Console.WriteLine(BoardBaseConversion.Board64to120[1]);
+        }
+    }
+}
