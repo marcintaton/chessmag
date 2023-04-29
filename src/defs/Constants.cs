f79e5992a08ff39fc4841dcf77a6c7f38dd07761
MArcin Tatoń <tatonmarcinpr@gmail.com>
2023-04-29 23:08:36 +0200

    added custom FEN for later

--- src/defs/Constants.cs
+++ src/defs/Constants.cs
@@ -7,6 +7,6 @@ namespace chessmag.src.defs
         public const string StartingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         public const string TestFEN1 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
         public const string TestFEN2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
-        public const string TestFEN3 = "3K4/3P4/3p4/8/8/7k/8/8 w - - 0 1";
+        public const string TestFEN3 = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
     }
 }
