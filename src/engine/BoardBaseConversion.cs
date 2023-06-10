be90b3b623377723ad8bc6cd02f2854c81cd7c05
Marcin Tatoń <tatonmarcinpr@gmail.com>
2023-06-10 18:30:09 +0200

    positional values for evaluation

--- src/engine/BoardBaseConversion.cs
+++ src/engine/BoardBaseConversion.cs
@@ -12,6 +12,14 @@ namespace chessmag.engine
 
         public static int[] Sq120ToFile { get; } = Enumerable.Repeat(65, Constants.BoardSize).ToArray();
         public static int[] Sq120ToRank { get; } = Enumerable.Repeat(65, Constants.BoardSize).ToArray();
+        public static int[] Mirror64 { get; } = new int[]{ 56,57,58,59,60,61,62,63,
+                                                           48,49,50,51,52,53,54,55,
+                                                           40,41,42,43,44,45,46,47,
+                                                           32,33,34,35,36,37,38,39,
+                                                           24,25,26,27,28,29,30,31,
+                                                           16,17,18,19,20,21,22,23,
+                                                           8,9,10,11,12,13,14,15,
+                                                           0,1,2,3,4,5,6,7 };
 
         public static int FrTo64(int file, int rank)
         {
@@ -27,14 +35,6 @@ namespace chessmag.engine
         public static bool IsOffboard(int sq120)
         {
             return Board120to64[sq120] == 65;
-        }
-
-        public static int Mirror64(int sq64)
-        {
-            int rank = Sq120ToRank[Board64to120[sq64]];
-            int file = Sq120ToFile[Board64to120[sq64]];
-
-            return 0;
         }
 
         static BoardBaseConversion()
