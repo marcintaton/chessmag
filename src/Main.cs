17bf0a8127c5c8d5394177ad8fd9c2f210ebd2e1
Marcin Tatoń <tatonmarcinpr@gmail.com>
2023-06-18 15:32:30 +0200

    clearing usings

--- src/Main.cs
+++ src/Main.cs
@@ -1,10 +1,4 @@
-﻿using System.Diagnostics;
-using chessmag.defs;
-using chessmag.engine;
-using chessmag.protocols.UCI;
-using chessmag.protocols.xBoard;
-using chessmag.tests;
-using chessmag.utils;
+﻿using chessmag.protocols.xBoard;
 
 namespace chessmag
 {
